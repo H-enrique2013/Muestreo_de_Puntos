4a6defaed3480ed33d9b47683aaae95f6b58ddd8
Enrique <hsandovals@uni.pe>
2024-06-29 17:46:10 -0500

    Nuevos Cambios 29

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ app = Flask(__name__)
 def index():
     return "API de generación de mapas está activa."
 
-@app.route('/generate-map', methods=['POST'])
+@app.route('/generate-map', methods=['GET', 'POST'])
 def generate_map():
-    data = request.get_json()
-    dep = data.get('dep')
-    prov = data.get('prov')
-    distr = data.get('distr')
-    dicPuntos = script_mapa.generar_puntos()
-    script_mapa.GeneradorHmtl_mapa(dep, prov, distr, dicPuntos)
-    return jsonify({"status": "success", "message": "Mapa generado exitosamente"})
+    if request.method=='POST':
+        data = request.get_json()
+        dep = data.get('dep')
+        prov = data.get('prov')
+        distr = data.get('distr')
+        dicPuntos = script_mapa.generar_puntos()
+        script_mapa.GeneradorHmtl_mapa(dep, prov, distr, dicPuntos)
+        return jsonify({"status": "success", "message": "Mapa generado exitosamente"}),200
+    else:
+        return jsonify({"error":"Método no permitido"}),405
 
 @app.route('/favicon.ico')
 def favicon():
