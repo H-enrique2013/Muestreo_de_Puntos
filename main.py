0cf7a5fa1a98ab996a0391086e7eafd345eb4a02
Enrique <hsandovals@uni.pe>
2024-06-30 03:32:27 -0500

    cambios en index

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 from flask import Flask, request, jsonify, send_from_directory,render_template
 import script_mapa
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='templates')
+
 
 # Ruta principal que renderiza el template index.html
 @app.route('/')
 def index():
-    json_data = {
-        "Departamento": "AYACUCHO",
-        "Provincia": "HUANTA",
-        "Distrito": "IGUAIN",
-        "DiccionarioPuntos": {
-            "Punto 1": [-74.22177636647999, -12.990092382255995],
-            "Punto 2": [-74.2169500514367, -12.96851318157424],
-            "Punto 3": [-74.22122551977878, -12.983558205695665],
-            "Punto 4": [-74.22264087758928, -12.980474113395678],
-            "Punto 5": [-74.21986777821412, -12.975849290825769],
-            "Punto 6": [-74.22186511449841, -12.974253850116721],
-            "Punto 7": [-74.2184392018611, -12.99043919073571],
-            "Punto 8": [-74.2202314606623, -12.968993352057701],
-            "Punto 9": [-74.21809553370747, -12.976150035827443],
-            "Punto 10": [-74.22237603049598, -12.989333931691757],
-            "Punto 11": [-74.22196046558193, -12.996294387451847]
-        }
-    }
-    return render_template('index.html', title='API de generación de Coordenadas está activa.', json_data=json_data)
+    
+    return render_template('index.html')
 
 @app.route('/generate-map', methods=['GET', 'POST'])
 def generate_map():
