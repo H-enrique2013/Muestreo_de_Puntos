480eb6a0c5d4bb652b2d2dcf7782a5947d7f3ae0
Enrique <hsandovals@uni.pe>
2024-06-30 03:08:20 -0500

    Nuevos cambios 30/06/2024

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory,render_template
 import script_mapa
 
 app = Flask(__name__)
 
+# Ruta principal que renderiza el template index.html
 @app.route('/')
 def index():
-    return "API de generación de mapas está activa."
+    json_data = {
+        "Departamento": "AYACUCHO",
+        "Provincia": "HUANTA",
+        "Distrito": "IGUAIN",
+        "DiccionarioPuntos": {
+            "Punto 1": [-74.22177636647999, -12.990092382255995],
+            "Punto 2": [-74.2169500514367, -12.96851318157424],
+            "Punto 3": [-74.22122551977878, -12.983558205695665],
+            "Punto 4": [-74.22264087758928, -12.980474113395678],
+            "Punto 5": [-74.21986777821412, -12.975849290825769],
+            "Punto 6": [-74.22186511449841, -12.974253850116721],
+            "Punto 7": [-74.2184392018611, -12.99043919073571],
+            "Punto 8": [-74.2202314606623, -12.968993352057701],
+            "Punto 9": [-74.21809553370747, -12.976150035827443],
+            "Punto 10": [-74.22237603049598, -12.989333931691757],
+            "Punto 11": [-74.22196046558193, -12.996294387451847]
+        }
+    }
+    return render_template('index.html', title='API de generación de Coordenadas está activa.', json_data=json_data)
 
 @app.route('/generate-map', methods=['GET', 'POST'])
 def generate_map():
     if request.method=='POST':
         data = request.get_json()
-        dep = data.get('dep')
-        prov = data.get('prov')
-        distr = data.get('distr')
-        dicPuntos = script_mapa.generar_puntos()
+        dep = data.get('Departamento')
+        prov = data.get('Provincia')
+        distr = data.get('Distrito')
+        #dicPuntos = script_mapa.generar_puntos()
+        dicPuntos=data.get('DiccionarioPuntos')
         script_mapa.GeneradorHmtl_mapa(dep, prov, distr, dicPuntos)
         # Verificar si el resultado es un JSON válido
         try:
@@ -25,9 +45,9 @@ def generate_map():
         
         
     else:
-        metodo=request.method
-        #return jsonify({"error":"Método no permitido"}),405
-        return jsonify({"error":metodo}),405
+        #metodo=request.method
+        return jsonify({"error":"Método no permitido"}),405
+        #return jsonify({"error":metodo}),405
 
 @app.route('/favicon.ico')
 def favicon():
@@ -35,3 +55,4 @@ def favicon():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
--- script_mapa.py
+++ script_mapa.py
@@ -2,28 +2,17 @@ import geopandas as gpd
 import folium
 from shapely.geometry import Point
 import random
+import os
 
-
-
-dir_shape={"AYACUCHO":"Shape/Ayacucho/05_AYACUCHO_SectoresEstadisticos.shp",
-           "APURIMAC":"Shape/Apurimac/03_APURIMAC_SectoresEstadisticos.shp",
-           "HUANCAVELICA":"Shape/Huancavelica/09_HUANCAVELICA_SectoresEstadisticos.shp",
-           "MADRE DE DIOS":"Shape/Madre de Dios/17_MADRE_DE_DIOS_SectoresEstadisticos.shp",
-           "MOQUEGUA":"Shape/Moquegua/18_MOQUEGUA_SectoresEstadisticos.shp",
-           "PUNO":"Shape/Puno/21_PUNO_SectoresEstadisticos.shp",
-           "TACNA":"Shape/Tacna/23_TACNA_SectoresEstadisticos.shp",
-           "TUMBES":"Shape/Tumbes/24_TUMBES_SectoresEstadisticos.shp",
+dir_shape={"AYACUCHO":os.path.join('Shape','Ayacucho','05_AYACUCHO_SectoresEstadisticos.shp'),
+           "APURIMAC":os.path.join('Shape','Apurimac','03_APURIMAC_SectoresEstadisticos.shp'),
+           "HUANCAVELICA":os.path.join('Shape','Huancavelica','09_HUANCAVELICA_SectoresEstadisticos.shp'),
+           "MADRE DE DIOS":os.path.join('Shape','Madre de Dios','17_MADRE_DE_DIOS_SectoresEstadisticos.shp'),
+           "MOQUEGUA":os.path.join('Shape','Moquegua','18_MOQUEGUA_SectoresEstadisticos.shp'),
+           "PUNO":os.path.join('Shape','Puno','21_PUNO_SectoresEstadisticos.shp'),
+           "TACNA":os.path.join('Shape','Tacna','23_TACNA_SectoresEstadisticos.shp'),
+           "TUMBES":os.path.join('Shape','Tumbes','24_TUMBES_SectoresEstadisticos.shp')
            }
-
-# Generar los 11 puntos
-def generar_puntos():
-    dicPuntos = {}
-    for i in range(1, 12):
-        indice = "Punto " + str(i)
-        random_longitud = random.uniform(-74.216694960800000, -74.22389061599995)
-        random_latitud = random.uniform(-12.966311839999946, -12.999319500999945)
-        dicPuntos[indice] = [random_longitud, random_latitud]
-    return dicPuntos
 
 def GeneradorHmtl_mapa(dep,prov,distr,dicPuntos):
     file_shape=dir_shape[dep]
@@ -53,3 +42,4 @@ def GeneradorHmtl_mapa(dep,prov,distr,dicPuntos):
     m.save("mapa.html")
 
 
+#GeneradorHmtl_mapa('AYACUCHO','HUANTA','IGUAIN',generar_puntos())
