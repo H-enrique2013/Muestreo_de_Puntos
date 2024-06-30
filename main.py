d69a4f50562253b003e5e0f7203aae3c4343e2f3
Enrique <hsandovals@uni.pe>
2024-06-30 17:15:51 -0500

    Cambios nuevos en main

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
-from flask import Flask, request, jsonify, send_from_directory,render_template
+from flask import Flask, request, jsonify, send_from_directory, render_template
 import script_mapa
 import os
+import time
+import threading
 
 app = Flask(__name__, template_folder='templates')
+
+
+def delete_file(filepath):
+    time.sleep(5)  # Esperar 5 segundos
+    if os.path.exists(filepath):
+        try:
+            os.remove(filepath)
+        except Exception as e:
+            print(f"Error al eliminar el archivo: {str(e)}")
 
 
 # Ruta principal que renderiza el template index.html
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
 @app.route('/generate-map', methods=['POST'])
 def generate_map():
-    if request.method=='POST':
+    if request.method == 'POST':
         data = request.get_json()
         dep = data.get('Departamento')
         prov = data.get('Provincia')
         distr = data.get('Distrito')
-        dicPuntos=data.get('DiccionarioPuntos')
+        dicPuntos = data.get('DiccionarioPuntos')
 
+        filepath = None
         try:
             filepath = script_mapa.GeneradorHmtl_mapa(dep, prov, distr, dicPuntos)
             # Devolver el archivo como respuesta
-            return send_from_directory(directory='static', filename=filepath.split('/')[-1]), 200
+            filename = os.path.basename(filepath)
+            directory = os.path.dirname(filepath)
+            response = send_from_directory(directory=directory, path=filename)
+            # Iniciar un hilo para eliminar el archivo después de enviar la respuesta
+            threading.Thread(target=delete_file, args=(filepath,)).start()
+            return response, 200
         except Exception as e:
+            print(f"Error: {str(e)}")  # Agregar esto para imprimir el error en la consola
             return jsonify({"error": f"Error al generar el mapa: {str(e)}"}), 500
-        finally:
-            # Eliminar el archivo después de enviar la respuesta
-            if os.path.exists(filepath):
-                os.remove(filepath)
- 
+
     else:
-        #metodo=request.method
-        return jsonify({"error":"Método no permitido"}),405
-        #return jsonify({"error":metodo}),405
+        return jsonify({"error": "Método no permitido"}), 405
 
 @app.route('/favicon.ico')
 def favicon():
@@ -42,4 +53,3 @@ def favicon():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
